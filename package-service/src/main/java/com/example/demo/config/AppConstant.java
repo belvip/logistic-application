f0faf5fec5d10eacb727560e46a920853929fc83
belvip <pouadjeubelvi@gmail.com>
2025-07-16 11:18:38 +0000

    feat package config added

---
+++ package-service/src/main/java/com/example/demo/config/AppConstant.java
@@ -0,0 +1,13 @@
+package com.example.demo.config;
+
+public class AppConstant {
+    
+    private AppConstant() {
+        // Private constructor to hide the implicit public one
+    }
+    
+    public static final String PAGE_NUMBER = "0";
+    public static final String PAGE_SIZE = "10";
+    public static final String SORT_PACKAGE_BY = "packageId";
+    public static final String SORT_DIR = "asc";
+}
